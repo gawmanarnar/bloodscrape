2d45f0266e0ba9e7638f8b86d77cbee1553f7204
Tyler Gaw <tylerjgaw@gmail.com>
2021-02-27 20:14:31 -0500

    Fixed pipeline by running cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,23 @@
 extern crate reqwest;
-extern crate table_extract;
 extern crate scraper;
+extern crate table_extract;
 
 fn main() {
     find("https://www.wowprogress.com/gearscore/us/char_rating/prev/1/lfg.1/raids_week./lang.en/class.paladin?sortby=ts");
+}
+
+fn get_character(row: &table_extract::Row) -> String {
+    let fragment = scraper::Html::parse_fragment(row.get("Character").unwrap());
+    let selector = scraper::Selector::parse("a").unwrap();
+
+    let atag = fragment.select(&selector).next().unwrap();
+    return String::from(atag.text().next().unwrap());
 }
 
 fn find(url: &str) {
     let body = reqwest::blocking::get(url).unwrap().text().unwrap();
     let table = table_extract::Table::find_first(&body).unwrap();
     for row in &table {
-        let fragment = scraper::Html::parse_fragment(row.get("Character").unwrap());
-        let selector = scraper::Selector::parse("a").unwrap();
-
-        let a = fragment.select(&selector).next().unwrap();
-        println!("{}", a.text().next().unwrap());
+        println!("{}", get_character(&row));
     }
 }
