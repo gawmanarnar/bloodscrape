3149d370ab030df0bf0e6b01ac1aff7bb191e755
Tyler Gaw <tylerjgaw@gmail.com>
2021-03-29 23:20:27 -0400

    simplified parsing by creating get_content function

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,9 @@ fn main() {
     find("https://www.wowprogress.com/gearscore/us/char_rating/prev/1/lfg.1/raids_week./lang.en/class.paladin?sortby=ts");
 }
 
-fn get_atag(cell: &str) -> String {
+fn get_content(cell: &str, tag: &str) -> String {
     let fragment = scraper::Html::parse_fragment(cell);
-    let selector = scraper::Selector::parse("a").unwrap();
+    let selector = scraper::Selector::parse(tag).unwrap();
 
     let option = fragment.select(&selector).next();
     if option.is_none() {
@@ -19,37 +19,19 @@ fn get_atag(cell: &str) -> String {
 }
 
 fn get_character_name(cell: &str) -> String {
-    return get_atag(cell);
+    return get_content(cell, "a");
 }
 
 fn get_guild_name(cell: &str) -> String {
-    let fragment = scraper::Html::parse_fragment(cell);
-    let selector = scraper::Selector::parse("nobr").unwrap();
-
-    let nobr_option = fragment.select(&selector).next();
-    if nobr_option.is_none() {
-        return String::new();
-    }
-
-    let nobr = nobr_option.unwrap();
-    return String::from(nobr.text().next().unwrap());
+    return get_content(cell, "nobr");
 }
 
 fn get_realm(cell: &str) -> String {
-    return get_atag(cell);
+    return get_content(cell, "a");
 }
 
 fn get_posted_time(cell: &str) -> String {
-    let fragment = scraper::Html::parse_fragment(cell);
-    let selector = scraper::Selector::parse("span").unwrap();
-
-    let nobr_option = fragment.select(&selector).next();
-    if nobr_option.is_none() {
-        return String::new();
-    }
-
-    let nobr = nobr_option.unwrap();
-    return String::from(nobr.text().next().unwrap());
+    return get_content(cell, "span");
 }
 
 fn find(url: &str) {
@@ -57,7 +39,7 @@ fn find(url: &str) {
     let table = table_extract::Table::find_first(&body).unwrap();
     for row in &table {
         let mut index = 0;
-        println!("");
+        println!();
         for cell in row.iter() {
             match index {
                 0 => println!("Character: {:#?}", get_character_name(cell)),
