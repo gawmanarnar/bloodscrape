53dc210d3e37a7e6b664da1312b1b6e9b0022750
Tyler Gaw <tylerjgaw@gmail.com>
2021-03-29 22:41:00 -0400

    added get_guild_name

--- src/main.rs
+++ src/main.rs
@@ -13,14 +13,29 @@ fn get_character_name(cell: &std::string::String) -> String {
     return String::from(atag.text().next().unwrap());
 }
 
+fn get_guild_name(cell: &std::string::String) -> String {
+    let fragment = scraper::Html::parse_fragment(cell);
+    let selector = scraper::Selector::parse("nobr").unwrap();
+
+    let nobr_option = fragment.select(&selector).next();
+    if nobr_option.is_none() {
+        return String::new();
+    }
+
+    let nobr = nobr_option.unwrap();
+    return String::from(nobr.text().next().unwrap());
+}
+
 fn find(url: &str) {
     let body = reqwest::blocking::get(url).unwrap().text().unwrap();
     let table = table_extract::Table::find_first(&body).unwrap();
     for row in &table {
         let mut index = 0;
         for cell in row.iter() {
+            //println!("{:#?}", cell);
             match index {
                 0 => println!("{:#?}", get_character_name(cell)),
+                1 => println!("{:#?}", get_guild_name(cell)),
                 _ => continue,
             }
             index += 1;
