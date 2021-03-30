72cfd22fc00e6b4c4c6b7901b184a0b4680c24d1
Tyler Gaw <tylerjgaw@gmail.com>
2021-03-29 22:10:58 -0400

    working character name parsing

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
 extern crate reqwest;
 extern crate scraper;
-extern crate table_extract;
 
 fn main() {
     find("https://www.wowprogress.com/gearscore/us/char_rating/prev/1/lfg.1/raids_week./lang.en/class.paladin?sortby=ts");
 }
 
-fn get_character(row: &table_extract::Row) -> String {
-    let fragment = scraper::Html::parse_fragment(row.get("Character").unwrap());
+fn get_character_name(cell: &std::string::String) -> String {
+    let fragment = scraper::Html::parse_fragment(cell);
     let selector = scraper::Selector::parse("a").unwrap();
 
     let atag = fragment.select(&selector).next().unwrap();
@@ -18,6 +17,13 @@ fn find(url: &str) {
     let body = reqwest::blocking::get(url).unwrap().text().unwrap();
     let table = table_extract::Table::find_first(&body).unwrap();
     for row in &table {
-        println!("{}", get_character(&row));
+        let mut index = 0;
+        for cell in row.iter() {
+            match index {
+                0 => println!("{:#?}", get_character_name(cell)),
+                _ => continue,
+            }
+            index += 1;
+        }
     }
 }
