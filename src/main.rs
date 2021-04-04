95cbb3725888bfedb041574652fa869ec6e2aaf5
Tyler Gaw <tylerjgaw@gmail.com>
2021-04-04 14:22:40 -0400

    generate warcraft log urls for characters pulled from wow-progress lfg

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,31 @@
 extern crate dotenv_codegen;
 extern crate reqwest;
 extern crate scraper;
+extern crate serde;
+
+#[derive(Default, Debug)]
+struct Character {
+    name: String,
+    guild: String,
+    realm: String,
+    ilvl: f32,
+    time: String,
+}
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     let arg = &args[1];
 
-    println!("{}", dotenv!("WARCRAFTLOGS_API_KEY"));
+    let wowprogress_url = format!("https://www.wowprogress.com/gearscore/us/char_rating/prev/1/lfg.1/raids_week./lang.en/class.{class}?sortby=ts", 
+                                    class = arg);
 
-    find(&format!("https://www.wowprogress.com/gearscore/us/char_rating/prev/1/lfg.1/raids_week./lang.en/class.{class}?sortby=ts", class = arg));
+    let mut characters: Vec<Character> = Vec::new();
+    find_characters(&wowprogress_url, &mut characters);
+
+    let api_key = dotenv!("WARCRAFTLOGS_API_KEY");
+    for char in &mut characters {
+        get_logs(char, api_key);
+    }
 }
 
 fn get_content(cell: &str, tag: &str) -> String {
@@ -41,20 +58,48 @@ fn get_posted_time(cell: &str) -> String {
     get_content(cell, "span")
 }
 
-fn find(url: &str) {
+fn get_ilvl(cell: &str) -> f32 {
+    let float = cell.parse::<f32>();
+    if !float.is_ok() {
+        return 0.0;
+    }
+    return float.unwrap();
+}
+
+fn make_character(entry: &table_extract::Row) -> Character {
+    let mut character: Character = Default::default();
+    for (index, cell) in entry.iter().enumerate() {
+        match index {
+            0 => character.name = get_character_name(cell),
+            1 => character.guild = get_guild_name(cell),
+            3 => character.realm = get_realm(cell),
+            4 => character.ilvl = get_ilvl(cell),
+            5 => character.time = get_posted_time(cell),
+            _ => (),
+        }
+    }
+
+    return character;
+}
+
+fn process_realm(realm: &str) -> String {
+    return realm.replace(' ', "-").replace('\'', "").to_lowercase();
+}
+
+fn get_logs(character: &Character, api_key: &str) {
+    let warcraftlogs_url = format!("https://www.warcraftlogs.com:443/v1/rankings/character/{character_name}/{realm}/{region}?api_key={key}",
+                                    character_name = character.name.to_lowercase(),
+                                    realm = process_realm(&character.realm),
+                                    region = "us",
+                                    key = api_key);
+
+    println!("{}", warcraftlogs_url);
+}
+
+fn find_characters(url: &str, characters: &mut Vec<Character>) {
     let body = reqwest::blocking::get(url).unwrap().text().unwrap();
     let table = table_extract::Table::find_first(&body).unwrap();
     for row in &table {
-        println!();
-        for (index, cell) in row.iter().enumerate() {
-            match index {
-                0 => println!("Character: {:#?}", get_character_name(cell)),
-                1 => println!("Guild Name: {:#?}", get_guild_name(cell)),
-                3 => println!("Realm: {:#?}", get_realm(cell)),
-                4 => println!("Item Level: {:#?}", cell),
-                5 => println!("Posted: {:#?}", get_posted_time(cell)),
-                _ => (),
-            }
-        }
+        characters.push(make_character(&row));
     }
 }
